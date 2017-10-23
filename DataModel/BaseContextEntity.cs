611ddc52f71ed9a68d14daa45dcdf3ca7af7c934
Tjaart Blignaut <tjaartblig@gmail.com>
2017-10-23 10:46:47 +0200

    add base data model

---
+++ DataModel/BaseContextEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyBucks.Core.Model.DataModel
+{
+    public class BaseContextEntity : BaseEntity
+    {
+        public string Context { get; set; }
+    }
+}
---
+++ DataModel/BaseEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyBucks.Core.Model.DataModel
+{
+    public class BaseEntity
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        [Key]
+        public long RefId { get; set; }
+
+        public override string ToString()
+        {
+            return $"RefId :{RefId}";
+        }
+    }
+}
