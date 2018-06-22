aa1f4a122a81788f0eddc5ddbeea5c826a4e0d04
Tjaart Blignaut <tjaartblig@gmail.com>
2018-06-22 12:35:27 +0200

    make object list accessible inpaged replort reply

--- PaginatedListReply.cs
+++ PaginatedListReply.cs
@@ -1,8 +1,14 @@
-﻿namespace MyBucks.Core.Model
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace MyBucks.Core.Model
 {
+    
     public class PaginatedListReply<T> : ListReply<T>
     {
         public int TotalItems { get; set; }
         public int TotalPages { get; set; }
+
+        public List<object> ObjectList => base.ResultList.Cast<object>().ToList();
     }
 }
