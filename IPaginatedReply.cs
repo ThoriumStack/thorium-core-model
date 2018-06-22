0c8d0d10215eb7d778348f89b6e927ebb6bd8492
Tjaart Blignaut <tjaartblig@gmail.com>
2018-06-22 12:40:17 +0200

    add interface to make paging parms accessible without generic access

---
+++ IPaginatedReply.cs
@@ -0,0 +1,8 @@
+﻿namespace MyBucks.Core.Model
+{
+    public interface IPaginatedReply
+    {
+        int TotalItems { get; set; }
+        int TotalPages { get; set; }
+    }
+}
--- PaginatedListReply.cs
+++ PaginatedListReply.cs
@@ -4,11 +4,9 @@ using System.Linq;
 namespace MyBucks.Core.Model
 {
     
-    public class PaginatedListReply<T> : ListReply<T>
+    public class PaginatedListReply<T> : ListReply<T>, IPaginatedReply
     {
         public int TotalItems { get; set; }
         public int TotalPages { get; set; }
-
-        public List<object> ObjectList => base.ResultList.Cast<object>().ToList();
     }
 }
