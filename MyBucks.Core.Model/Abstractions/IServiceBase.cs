da5020472969686a1c762c1d21858c7b61d7a086
Ruan Strydom <ruans@mybucks.com>
2018-07-31 08:21:52 +0200

    Added IServiceBase

---
+++ MyBucks.Core.Model/Abstractions/IServiceBase.cs
@@ -0,0 +1,8 @@
+﻿namespace MyBucks.Core.Model.Abstractions
+{
+    public interface IServiceBase
+    {
+        int CurrentUserId { get; set; }
+        string CurrentContext { get; set; }
+    }
+}
